24923748f934860c005e2f70035f46a085e5b331
meilindehuzhi <3111959806@qq.com>
2025-07-03 21:38:06 +0800

    test 7.3

--- summer study/summer study/test1.cpp
+++ summer study/summer study/test1.cpp
@@ -1,6 +1,26 @@
 #include<stdio.h>
+/*int main()
+{
+	//printf("hello summer");
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int i = 0;
+	int* p = &arr[0];
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	for (i = 0; i < sz; i++) {
+		printf("%d", *(p + i));
+	}
+	return 0;
+}*/
+int my_strlen(char *s)
+{
+	 char *p = s;
+	 while (*p != '\0')
+	 p++;
+	 return p - s;
+	}
+
 int main()
 {
-	printf("hello summer");
-	return 0;
-}
+	 printf("%d\n", my_strlen("abc"));
+		 return 0;
+	}
