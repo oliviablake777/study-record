ea72dcac567db875814c56a940b4a2d5f59f5e19
oliviablake777 <3111959806@qq.com>
2026-03-07 22:26:51 +0800

    AVL树

---
+++ AVL树/AVL树/AVLTree.h
@@ -0,0 +1,99 @@
+#pragma once
+template<class K,class V>
+struct AVLTreeNode
+{
+	pair<K, V> _kv;
+	AVLTreeNode<K, V>* _left;
+	AVLTreeNode<K, V>* _right;
+	AVLTreeNode<K, V>* _parent;
+	int _bf;
+	AVLTreeNode(const pair<K,V>& kv)
+		；_kv(kv)
+		,_left(nullptr)
+		,_right(nullptr)
+		,_parent(nullptr)
+		,_bf(0)
+	{ }
+};
+template<class K, class V>
+class AVLTree
+{
+	typedef AVLTreeNode<K, V> Node;
+public:
+	bool Insert(const pair<K, V>& kv)
+	{
+		if (_root == nullptr)
+		{
+			_root = new Node(kv);
+			return true;
+		}
+
+		Node* parent = nullptr;
+		Node* cur = _root;
+		while (cur)
+		{
+			if (cur->_kv.first < kv.first)
+			{
+				parent = cur;
+				cur = cur->_right;
+			}
+			else if (cur->_kv.first > kv.first)
+			{
+				parent = cur;
+				cur = cur->_left;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		cur = new Node(kv);
+		if (parent->_kv.first < kv.first)
+		{
+			parent->_right = cur;
+		}
+		else
+		{
+			parent->_left = cur;
+		}
+		// 链接父亲
+		cur->_parent = parent;
+
+		// 控制平衡
+		// 更新平衡因子
+		while (parent)
+		{
+			if (cur == parent->_left)
+				parent->_bf--;
+			else
+				parent->_bf++;
+
+			if (parent->_bf == 0)
+			{
+				break;
+			}
+			else if (parent->_bf == 1 || parent->_bf == -1)
+			{
+				cur = parent;
+				parent = parent->_parent;
+			}
+			else if (parent->_bf == 2 || parent->_bf == -2)
+			{
+				// 旋转
+
+				break;
+			}
+			else
+			{
+				assert(false);
+			}
+		}
+
+
+		return true;
+	}
+
+private:
+	Node* _root = nullptr;
+};
